1ba089db1dbc1a0463380ff8d1551fe0134515fb
ПК <slava_zen@mail.ru>
2023-05-19 23:15:56 +0500

    ru.job4j.collection.SimpleArrayList

--- src/main/java/ru/job4j/collection/SimpleArrayList.java
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -16,15 +16,14 @@ public class SimpleArrayList<T> implements SimpleList<T> {
     public void add(T value) {
         modCount++;
         if (container.length >= size) {
-            container = grow();
+            grow();
         }
         container[size++] = value;
     }
 
     @Override
     public T set(int index, T newValue) {
-        Objects.checkIndex(index, size);
-        T oldValue = container[index];
+        T oldValue = get(index);
         container[index] = newValue;
         return oldValue;
     }
@@ -32,8 +31,7 @@ public class SimpleArrayList<T> implements SimpleList<T> {
     @Override
     public T remove(int index) {
         modCount++;
-        Objects.checkIndex(index, size);
-        T result = container[index];
+        T result = get(index);
         System.arraycopy(container, index + 1, container, index, container.length - index - 1);
         container[container.length - 1] = null;
         size--;
@@ -70,16 +68,12 @@ public class SimpleArrayList<T> implements SimpleList<T> {
                 if (!hasNext()) {
                     throw new NoSuchElementException();
                 }
-                if (expectedModCount != modCount) {
-                    throw new ConcurrentModificationException();
-                }
                 return container[point++];
             }
         };
     }
 
-    private T[] grow() {
+    private void grow() {
         container = Arrays.copyOf(container, container.length * 2);
-        return container;
     }
 }
