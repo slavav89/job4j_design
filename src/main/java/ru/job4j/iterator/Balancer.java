92409689e2dd87cc8ca6f3bad2b1a797699c6a01
ПК <slava_zen@mail.ru>
2023-08-19 23:49:29 +0500

    ru.job4j.iterator.Balancer[#505083]

---
+++ src/main/java/ru/job4j/iterator/Balancer.java
@@ -0,0 +1,17 @@
+package ru.job4j.iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Balancer {
+    public static void split(List<ArrayList<Integer>> nodes, Iterator<Integer> source) {
+        int index = 0;
+        while (source.hasNext()) {
+            nodes.get(index++).add(source.next());
+            if (index == nodes.size()) {
+                index = 0;
+            }
+        }
+    }
+}
