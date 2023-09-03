0e08905a5447cecbb561baaecf4de875547de830
ПК <slava_zen@mail.ru>
2023-09-03 14:05:09 +0500

    ru.job4j.io.LogFilter[#252490]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -1,11 +1,11 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class LogFilter {
     private final String file;
@@ -15,18 +15,34 @@ public class LogFilter {
     }
 
     public List<String> filter() {
+        List<String> result = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-            in.lines()
+            result = in.lines()
                     .filter(s -> s.contains(" 404 "))
-                    .forEach(System.out::println);
+                    .collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return Collections.emptyList();
+        return result;
+    }
+
+    public void saveTo(String out) {
+        var data = filter();
+        try (PrintWriter pw = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(out)
+                ))) {
+            for (String s : data) {
+                pw.println(s);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter("data/log.txt");
         logFilter.filter().forEach(System.out::println);
+        new LogFilter("data/log.txt").saveTo("data/404.txt");
     }
 }
