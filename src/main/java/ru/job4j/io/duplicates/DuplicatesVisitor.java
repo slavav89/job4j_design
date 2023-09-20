996668515e12681a0cec7535f0ea3cc186cd4b7f
ПК <slava_zen@mail.ru>
2023-09-20 14:40:07 +0500

    ru.job4j.io.duplicates.DuplicatesFinder[#315066]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -8,33 +8,24 @@ import java.util.*;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
     private final Map<FileProperty, List<Path>> map = new HashMap<>();
-    private final HashSet<FileProperty> set = new HashSet<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        if (Files.isRegularFile(file)) {
-            File f = file.toFile();
-            FileProperty fp = new FileProperty(f.length(), f.getName());
-            List<Path> list;
-            if (map.containsKey(fp)) {
-                set.add(fp);
-                list = map.get(fp);
-                list.add(file.toAbsolutePath());
-                map.replace(fp, list);
-            } else {
-                list = new ArrayList<>();
-                list.add(file.toAbsolutePath());
-                map.put(fp, list);
-            }
-        }
+        File f = file.toFile();
+        FileProperty fp = new FileProperty(f.length(), f.getName());
+        map.computeIfAbsent(fp, a -> new ArrayList<>());
+        map.get(fp).add(file);
         return super.visitFile(file, attrs);
     }
 
     public void printDuplicates() {
-        for (var fp : set) {
-            System.out.printf("%s - %d" + System.lineSeparator(), fp.getName(), fp.getSize());
-            for (var path : map.get(fp)) {
-                System.out.println(path);
+        for (var fp : map.keySet()) {
+            var list = map.get(fp);
+            if (list.size() > 1) {
+                System.out.printf("%s - %d" + System.lineSeparator(), fp.getName(), fp.getSize());
+                for (var path : list) {
+                    System.out.println(path);
+                }
             }
         }
     }
