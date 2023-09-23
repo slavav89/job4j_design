a82f2e9771c584ac487266cb1ed14ed6d7709873
ПК <slava_zen@mail.ru>
2023-09-23 15:39:23 +0500

    ru.job4j.io.Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,13 +9,26 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        validate(args);
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void validate(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("The program must have two parameters");
+        }
+        if (!Files.isDirectory(Paths.get(args[0]))) {
+            throw new IllegalArgumentException("Usage  ROOT_FOLDER.");
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException("The extension should start with \".\"");
+        }
     }
 }
