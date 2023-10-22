93d43d668652cd41d4bad020084442c68cfc66f2
ПК <slava_zen@mail.ru>
2023-10-22 12:05:47 +0500

    ru.job4j.io.Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,15 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        byte b = 1;
+        short s = 130;
+        int i = 35000;
+        long l = 2247483647L;
+        float f = 7.1F;
+        double d = 20.3;
+        boolean bool = true;
+        char c = 'c';
+        LOG.debug("There are 8 primitive types in java : byte = {}, short = {}, int = {}, long = {}, "
+                + "float = {}, double = {}, boolean = {}, char = {}.", b, s, i, l, f, d, bool, c);
     }
 }
