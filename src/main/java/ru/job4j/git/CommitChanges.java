c9dd1b8d9a926fb3e3e7ecafdc7f5a6e37532aab
ПК <slava_zen@mail.ru>
2024-06-02 22:33:37 +0500

    3.1. Добавить изменения в последний коммит. [#504829]

---
+++ src/main/java/ru/job4j/git/CommitChanges.java
@@ -0,0 +1,10 @@
+package ru.job4j.git;
+
+public class CommitChanges {
+    /**
+     * Hello world -> Hello everybody
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello everybody");
+    }
+}
